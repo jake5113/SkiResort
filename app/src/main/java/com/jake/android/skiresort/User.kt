9f8f08d47580311eafa50d5add951e7ac18d9a1b
jongwon5113 <jongwon5113@gmail.com>
2022-04-11 23:58:29 +0900

    This is Test.

---
+++ app/src/main/java/com/jake/android/skiresort/User.kt
@@ -0,0 +1,6 @@
+package com.jake.android.skiresort
+
+class User (val profile: Int, val name: String, val age: String, val greet: String){
+
+
+}
---
+++ app/src/main/java/com/jake/android/skiresort/UserAdapter.kt
@@ -0,0 +1,42 @@
+package com.jake.android.skiresort
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class UserAdapter(val context : FragmentList, val UserList: ArrayList<User>) : BaseAdapter() {
+    override fun getCount(): Int {
+        return UserList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return UserList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getView(position: Int, converview: View?, parent: ViewGroup?): View {
+        val view: View = LayoutInflater.from(context!!.context).inflate(R.layout.list_item_user, null)
+
+        val profile = view.findViewById<ImageView>(R.id.tv_ski)
+        val name = view.findViewById<TextView>(R.id.tv_name)
+        val greet = view.findViewById<TextView>(R.id.tv_greet)
+        val age = view.findViewById<TextView>(R.id.tv_age)
+
+        val user = UserList[position]
+
+        profile.setImageResource(user.profile)
+        name.text = user.name
+        age.text = user.age
+        greet.text = user.greet
+
+        return view
+    }
+
+}
